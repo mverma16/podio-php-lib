f231bd48a4ddcbd0337612c20c5b43fa148170c6
Manish Verma <manish.verma@ladybirdweb.com>
2016-07-05 13:18:27 +0530

    Route middleware patch

--- routes.php
+++ routes.php
@@ -1,10 +1,11 @@
 <?php
 
-Route::group(['middleware' => 'web'], function () {
+Route::group(['middleware' => 'web', 'middleware' => 'roles', 'middleware' => 'auth'], function () {
     Route::get('podio/settings', ['as' => 'podio-settings', 'uses' => 'App\Plugins\Podio\Controllers\SettingsController@index']);
     Route::post('post-podio-setting', ['as' => 'post-podio-setting', 'uses' => 'App\Plugins\Podio\Controllers\SettingsController@postSetting']);
     Route::get('podio/app-authentication', ['as' => 'podio-settings2',
         'uses'                                   => 'App\Plugins\Podio\Controllers\SettingsController@orgIndex', ]);
     Route::get('podio/space', ['as' => 'podio-settings3', 'uses' => 'App\Plugins\Podio\Controllers\SettingsController@spaceIndex']);
     Route::post('podio/create-app', ['as' => 'podio-settings4', 'uses' => 'App\Plugins\Podio\Controllers\SettingsController@createApp']);
+    Route::post('podio/app-auth', ['as' => 'podio-settings5', 'uses' => 'App\Plugins\Podio\Controllers\SettingsController@authApp']);
 });
