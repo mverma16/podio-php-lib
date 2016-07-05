910e5678a14f1a1c5a4553ba78032cb4c60de6f0
Manish Verma <manish.verma@ladybirdweb.com>
2016-07-05 18:38:29 +0530

    Updated route

--- routes.php
+++ routes.php
@@ -1,6 +1,6 @@
 <?php
 
-Route::group(['middleware' => 'web', 'middleware' => 'roles', 'middleware' => 'auth'], function () {
+Route::group(['middleware' => 'web'], function () {
     Route::get('podio/settings', ['as' => 'podio-settings', 'uses' => 'App\Plugins\Podio\Controllers\SettingsController@index']);
     Route::post('post-podio-setting', ['as' => 'post-podio-setting', 'uses' => 'App\Plugins\Podio\Controllers\SettingsController@postSetting']);
     Route::get('podio/app-authentication', ['as' => 'podio-settings2',
@@ -8,4 +8,5 @@ Route::group(['middleware' => 'web', 'middleware' => 'roles', 'middleware' => 'a
     Route::get('podio/space', ['as' => 'podio-settings3', 'uses' => 'App\Plugins\Podio\Controllers\SettingsController@spaceIndex']);
     Route::post('podio/create-app', ['as' => 'podio-settings4', 'uses' => 'App\Plugins\Podio\Controllers\SettingsController@createApp']);
     Route::post('podio/app-auth', ['as' => 'podio-settings5', 'uses' => 'App\Plugins\Podio\Controllers\SettingsController@authApp']);
+    Route::get('podio/create-item', ['as' => 'create.item', 'uses' => 'App\Plugins\Podio\Controllers\PodioController@createPodioTicket']);
 });
